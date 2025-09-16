753ed5e039eebadcedd7eb0d91c868a52e737e25
Andrej Kutny <andrej.kutny@gmail.com>
2025-09-16 18:13:55 +0200

    #24 added second 2nd return value to Solver abstact class

--- src/solvers/solver.py
+++ src/solvers/solver.py
@@ -1,5 +1,12 @@
+"""
+Abstract solver class - all solvers must inherit from this class.
+"""
 class Solver():
-    def solve(self, coordinates: list[tuple[float, float]])  -> list[tuple[float, float]]:
+    """
+    Solver main method with solve algorithm implementation.
+    Returns: list of coordinates and the distance traveled
+    """
+    def solve(self, coordinates: list[tuple[float, float]])  -> [list[tuple[float, float]], float]:
         raise NotImplementedError
 
     def mutate(self) -> bool:
