cd7ceff1b09691f83d10eb81cc7d0a7fae403894
Vladimir Giustacchini <giustacchini@yahoo.com>
2025-09-16 20:44:54 +0200

    added separate files for each cost function & fixed calculation errors

---
+++ src/cost_calculation/euclidean_cost_calculation.py
@@ -0,0 +1,5 @@
+from cost_calculation import CostCalculation
+
+class EuclideanCostCalculation(CostCalculation):
+    def calculate(self, pt1: tuple[float, float], pt2: tuple[float, float]) -> float:
+        return ((pt2[0] - pt1[0])**2 + (pt2[1] - pt1[1])**2)**0.5
