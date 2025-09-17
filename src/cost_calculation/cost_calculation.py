e6de9e2dbdd2115e1ba5ad6daeb2e37393562791
Vladimir Giustacchini <giustacchini@yahoo.com>
2025-09-17 16:43:41 +0200

    set class from cost_calculation.py as abstract base class

--- src/cost_calculation/cost_calculation.py
+++ src/cost_calculation/cost_calculation.py
@@ -1,3 +1,6 @@
-class CostCalculation():
+from abc import ABC, abstractmethod
+
+class CostCalculation(ABC):
+    @abstractmethod
     def calculate(self, pt1: tuple[float, float], pt2: tuple[float, float]) -> float:
         raise NotImplementedError
