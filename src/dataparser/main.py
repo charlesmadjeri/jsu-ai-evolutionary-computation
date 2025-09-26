e8da4c1309c2552b3df0a7b678d6f7c98d5cc41f
Vladimir Giustacchini <giustacchini@yahoo.com>
2025-09-26 19:23:10 +0200

    fix(dataparser): removed incompatible TSPLIB files,added unit test and fixed minor issues

--- src/dataparser/main.py
+++ src/dataparser/main.py
@@ -1,45 +1,43 @@
-# IMPORTANT!
-# This algorithm works for the moment only for TSP files that have NODE_COORD_SECTION. Ex: berlin52, burma14, d198 etc.
-# Files with EDGE_WEIGHT_SECTION and DISPLAY_DATA_SECTION (ex: bays29, brg180, si175 etc.) are not supported yet.
-
 import argparse
 import os
 import re
 
 def load_tsp_data(file_path: str) -> list[tuple[float, float]]:
-    if not file_path.endswith('.tsp'):
-        raise ValueError('File must be a .tsp file')
+    if not file_path.endswith(".tsp"):
+        raise ValueError("File must be a .tsp file")
     if not os.path.exists(file_path):
-        raise ValueError(f'File {file_path} does not exist')
+        raise ValueError(f"File {file_path} does not exist")
     
     node_coord_section = False
     nodes = []
 
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         while True:
             line = file.readline()
-            if line == "EOF\n":
+            if line == "EOF\n" or re.sub(" +", " ", line) == "\n": # in case no EOF is declared, ex: usa13509
                 break
             if node_coord_section == True:
-                nodes.append((re.sub(' +', ' ', line).strip().split(" ")[1], re.sub(' +', ' ', line).strip().split(" ")[2]))
+                nodes.append((re.sub(" +", " ", line).strip().split(" ")[1], re.sub(" +", " ", line).strip().split(" ")[2]))
             if line == "NODE_COORD_SECTION\n":
                 node_coord_section = True
-        return nodes
+    if nodes == []:
+        raise TypeError("Error: Could not retrieve a list of (x, y) coordinates. Try using a file with NODE_COORD_SECTION")
+    return nodes
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', type=str, required=True, dest='input_path',
-                        metavar='INPUT_TSP_PATH', 
-                        help='input dataset path')
-    parser.add_argument('-o', '--output', type=str, required=True, dest='output_path',
-                        metavar='OUTPUT_TSP_PATH', 
-                        help='output dataset export path')
+    parser.add_argument("-i", "--input", type=str, required=True, dest="input_path",
+                        metavar="INPUT_TSP_PATH", 
+                        help="input dataset path")
+    parser.add_argument("-o", "--output", type=str, required=True, dest="output_path",
+                        metavar="OUTPUT_TSP_PATH", 
+                        help="output dataset export path")
     args = parser.parse_args()
 
     loaded_data = load_tsp_data(args.input_path)
 
-    with open(args.output_path, 'w') as file:
+    with open(args.output_path, "w") as file:
         file.write("x,y\n")
         for x, y in loaded_data:
             file.write(f"{x},{y}\n")
